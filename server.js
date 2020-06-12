667fab8a95ace392c295e8e7191f66c71d228277
Sherri Booher <sherribooher74@gmail.com>
2020-06-12 11:01:45 -0400

    Configure Heroku

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ let sanitizeHTML = require("sanitize-html");
 let app = express();
 let db;
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.use(express.static("public"));
 
 let connectionString =
@@ -14,7 +19,7 @@ mongodb.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     db = client.db();
-    app.listen(3000);
+    app.listen(port);
   }
 );
 
